f85da8fd93055fda081273c53f1844d72aeb16e4
husteve07 <77590544+husteve07@users.noreply.github.com>
2024-09-10 23:13:04 -0400

    fixed spiral shooter bug
    added surprise

--- strm/scripts/spiral_laser_shooter.gd
+++ strm/scripts/spiral_laser_shooter.gd
@@ -1,24 +1,42 @@
 extends LaserShooter
 
-@export var num_of_lasers_to_shoot=5
+@export var num_of_lasers_to_shoot=0
 @export var ccw = true
 @export var range_of_spiral_deg = 180
 
+var flag = -1
 var external_timer = Timer.new()
 var internal_timer = Timer.new()
 var total_shooting_time = firing_time_interval - 0.01
-var time_interval_between_bullets = total_shooting_time/num_of_lasers_to_shoot
+
 var player_position_snapshot = Vector2.ZERO
 var current_laser_count = 0
 
-# Preload the red and purple laser scenes
-# var RedLaserScene = preload("res://scenes/Lasers/red_laser.tscn")
-# var BlueLaserScene = preload("res://scenes/Lasers/blue_laser.tscn")
+#external timer: every x second the shooter fires
+#
+#internal timer: y bullets shoots in z seconds with z/y seconds interval
+#
+#every bullet shoots at 180/y angle with starting angle pointed at the player
+#
+#External timer ends:
+#
+	#internal timer starts:
+		#
+		#at # bullet, fire at 0 + # angle
+		#
+		#check if its the last bullet, if not
+			#increment #
+		#else
+			#start external timer 
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var time_interval_between_bullets = total_shooting_time/num_of_lasers_to_shoot
 	internal_timer.connect("timeout", Callable(self, "spawn_laser_spiral"))
 	internal_timer.wait_time = time_interval_between_bullets;
+	print(time_interval_between_bullets)
 	add_child(internal_timer)
 
 	external_timer.connect("timeout", Callable(self, "reset_shooter"))
@@ -37,8 +55,10 @@ func spawn_laser_spiral():
 
 	print(current_laser_count)
 	var current_angle = range_of_spiral_deg/num_of_lasers_to_shoot * current_laser_count
+	flag *= -1
+	#laser_instance.direction = (player_position_snapshot - position).rotated(-deg_to_rad(flag*current_angle)).normalized()
 	if(ccw):
-		laser_instance.direction = (player_position_snapshot - position).rotated(deg_to_rad(-current_angle)).normalized()
+		laser_instance.direction = (player_position_snapshot - position).rotated(deg_to_rad(current_angle)).normalized()
 	else:
 		laser_instance.direction = (player_position_snapshot - position).rotated(deg_to_rad(current_angle)).normalized()
 
