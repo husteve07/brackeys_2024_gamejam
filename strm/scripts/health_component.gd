21f05d87bba12748602c67c6e130616fca8f4a3b
e22my <ethan.1.mooyoung@gmail.com>
2024-09-15 04:27:00 -0400

    Tutorial

--- strm/scripts/health_component.gd
+++ strm/scripts/health_component.gd
@@ -10,13 +10,9 @@ signal dead
 @export var max_health = 100
 @export var health_regen_countdown = 5;
 @export var health_regen_amount = 20
-@export var iframe_time = 1;
 
 var health_timer: Timer
 var flash_timer: Timer
-var i_frame_timer: Timer
-var current_shader_shit = 0
-var can_take_damage = true
 
 func _ready() -> void:
 	health_timer = Timer.new()
@@ -29,28 +25,7 @@ func _ready() -> void:
 	flash_timer.wait_time = 0.05
 	flash_timer.connect("timeout", Callable(self, "flash"))
 	add_child(flash_timer);
-	
-	i_frame_timer = Timer.new()
-	i_frame_timer.wait_time = iframe_time
-	i_frame_timer.connect("timeout", Callable(self, "on_i_frame_time_out"))
-	i_frame_timer.one_shot = true
-	add_child(i_frame_timer);
 	pass
-
-
-func start_i_frame_timer():
-	i_frame_timer.start();
-	print('start i frame')
-	flash_timer.start()
-	print('start flashing')
-
-
-func on_i_frame_time_out():
-	flash_timer.stop()
-	print('stop');	
-	get_parent().get_node("Animation Component").material.set_shader_parameter("flash_modifier", 0)
-	can_take_damage = true
-	print('iframe over')
 
 
 func on_health_regen_countdown_finish():
@@ -66,32 +41,20 @@ func on_health_regen_countdown_finish():
 #		then the dead signal is also fired
 func take_damage(damage: int):
 	#update UI 
-	#if(get_parent() as Player):
-		#get_parent().get_node("Animation Component").material.set_shader_parameter("flash_modifier", 1)
-	if(!can_take_damage):
-		print('damage_negated')
-		return;
-
-	start_i_frame_timer()
+	if(get_parent() as Player):
+		get_parent().get_node("Animation Component").material.set_shader_parameter("flash_modifier", 1)
+	flash_timer.start()
 	current_health = clampi(current_health  - damage, 0, max_health)
 	update_health.emit(current_health);
 	if(get_parent() as Player):
 		health_timer.start()
-	can_take_damage = false;
+	
 	#print("current Health: " + str(current_health))
 	if current_health == 0:
 		dead.emit();
-	
-		
 func flash():
 	if(get_parent() as Player):
-		if(current_shader_shit == 1):
-			get_parent().get_node("Animation Component").material.set_shader_parameter("flash_modifier", 0)
-			current_shader_shit = 0
-		else:
-			get_parent().get_node("Animation Component").material.set_shader_parameter("flash_modifier", 1)
-			current_shader_shit = 1
-
+		get_parent().get_node("Animation Component").material.set_shader_parameter("flash_modifier", 0)
 	
 func player_restore_health(restored_health):
 	current_health = clampi(current_health + restored_health, 0, max_health)
