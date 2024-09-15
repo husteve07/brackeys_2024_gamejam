e5d53676dbab2ca5e794fbba32bc04a87b114ec3
e22my <ethan.1.mooyoung@gmail.com>
2024-09-14 22:50:45 -0400

    CAN DETECT TILE

--- strm/scripts/skill_base.gd
+++ strm/scripts/skill_base.gd
@@ -4,10 +4,11 @@ class_name Skill
 @export var cool_down_seconds = 0
 @export var skill_damage = 50
 
+var tilemap: TileMapLayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	tilemap = get_tree().get_nodes_in_group("room")[0]
 
 func set_skill_activatable():
 	pass
@@ -22,7 +23,12 @@ func _process(delta: float) -> void:
 func activate(mouse_position: Vector2):
 	print("activating" + get_skill_name())
 	position = mouse_position
-	return true
+	var tile_mouse_pos = tilemap.local_to_map(mouse_position)
+	var tile = tilemap.get_cell_tile_data(tile_mouse_pos)
+	if tile:
+		if tile.get_custom_data("target"):
+			return true
+	return false
 	
 
 #helper func
